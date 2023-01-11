b73ceab328cf3c608abaf18c7698e0131be656de
bulkacity <112503952+bulkacity@users.noreply.github.com>
2023-01-11 14:50:26 -0500

    updated all code

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ require('dotenv').config()
 
 //connects to the database
 
+
+
 const connection = mysql.createConnection(
     {
         host: 'localhost',
@@ -21,6 +23,7 @@ const connection = mysql.createConnection(
 // connects to the mysql database
 
 connection.connect(function (err) {
+  
     if (err) return console.log(err);
     InquirerPrompt();
 })
@@ -39,9 +42,8 @@ const InquirerPrompt = () => {
                 'Add department',
                 'Add role',
                 'Add employee',
-                'Update all departments',
-                'Update employee infomation',
                 'View department budgets',
+                'Delete Employee',
                 'Exit'
             ]
         }
@@ -74,8 +76,8 @@ const InquirerPrompt = () => {
                 addEmployees();
             }
 
-            if (choices === "Update all departments") {
-                allDepartments();
+            if (choices === "Delete Employee") {
+                deleteEmp();
             }
 
             if (choices === "Update employee infomation") {
@@ -164,49 +166,57 @@ showEmployees = () => {
 
 //add roles infomation
 addRoles = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'roles',
-            message: "What do you want to add?",
-
-        },
-        {
-            type: 'input',
-            name: 'salary',
-            message: 'What is your yearly salary?',
-        }
-
-    ])
-        .then(answer => {
-            const parameters = [answer.roles, answer.salary];
-            const roles_var = `SELECT name, id FROM department`;
-
-            connection.query(roles_var, (err, data) => {
-                if (err) return console.log(err);
-                const department_var = data.map(({ name, id }) => ({ name: name, value: id }));
-
-                inquirer.prompt([
-                    {
-                        type: 'list',
-                        name: 'department_var',
-                        message: "What department is this role in?",
-                        choices: department_var
-                    }
-                ])
-                    .then(department_varChoice => {
-                        const department_var = department_varChoice.department_var;
-                        parameters.push(department_var);
-                        const mysql = `INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)`;
-
-                        connection.query(mysql, parameters, (err, result) => {
-                            if (err) return console.log(err);
-                            console.log('Added' + answer.roles + "to roles");
-                            showRoles();
-                        });
-                    });
-            });
+    connection.query(`SELECT * FROM departments`, (err, res) => {
+        if (err) throw err;
+        listOfDepartments = res.map(dept => (
+          {
+            name: dept.name,
+            value: dept.dept_id
+          }
+        ))
+        inquirer
+        .prompt([
+          {
+            type: "input",
+            name: "role_add",
+            message: "What is the name of the role you would like to add?",
+            validate: newRoleInput => {
+              if (newRoleInput) {
+                return true
+              } else {
+                console.log("Please enter a name for the new role");
+                return false
+              }
+            }
+          },
+          {
+            type: "number",
+            name: "salary",
+            message: "What is the salary for the role you would like to add?",
+            default: 10000
+          },
+          {
+            type: "list",
+            name: "deptId",
+            message: "What is the department for the role you would like to add?",
+            choices: listOfDepartments
+          }
+        ])
+        .then((answer) => {
+          console.log(' ');
+          connection.query("INSERT INTO Roles SET ?",
+            {
+              title: answer.role_add,
+              salary: answer.salary,
+              dept_id: answer.deptId,
+            },
+            (err, res) => {
+              if (err) throw err;
+              showRoles();
+            }
+          );
         });
+      })
 };
 
 
@@ -269,21 +279,18 @@ updateEmployee = () => {
 
 // View the budget
 viewBudget = () => {
-    const query = `SELECT department.id AS Dept_ID, department.name AS Department_Name, CONCAT('$', FORMAT(SUM(salary),0)) AS Budget 
-    FROM roles 
-    INNER JOIN employees USING (role_id)
-    INNER JOIN departments ON roles.dept_id = department.dept_id 
-    GROUP BY roles.dept_id;`;
-    connection.query(query, (err, res) => {
-      if (err) throw err;
-      console.log(` `);
-      
-      console.table(res);
+    const query = `SELECT departments.dept_id AS Dept_ID, departments.name AS Department_Name, CONCAT('$', FORMAT(SUM(salary),0)) AS Budget 
+  FROM roles 
+  INNER JOIN employees USING (role_id)
+  INNER JOIN departments ON roles.dept_id = departments.dept_id 
+  GROUP BY roles.dept_id;`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    newTransaction ();
+  })
+}
 
-      console.log(` `);
-      newTransaction();
-    })
-  }
 
 //Update/ADD Department
 addDepartments = () => {
@@ -299,49 +306,116 @@ addDepartments = () => {
             connection.query(mysql, answer.department, (err, results) => {
                 if (err) return console.log(err);
                 console.log('Added' + answer.department + "to departments");
-
                 showDepartments();
-                newTransaction();
             });
         });
 }
 //Add employee
 addEmployees = ()  => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'first_name',
-            message: 'Your First Name?',
-        },
-        {
-            type: 'input',
-            name: 'last_name',
-            message: 'Your Last Name?',
-        }
-    ])
-    .then(answer => {
-        const parameters = [answer.first_name, answer.last_name]
-        const roles_var = `SELECT roles.role_id, roles.title FROM roles`;
-
-        connection.query(roles_var, (err, data) => {
-            if(err) return console.log(err);
-            const roles = data.map(({ id, title }) => ({ name:title, value:id}));
-
+    
+        connection.query(`SELECT * FROM roles`, (err,res) => {
+            if (err) throw err;
+            listOfRoles = res.map(role => (
+              {
+                name: role.title,
+                value: role.role_id
+              }
+            ));
+            
             inquirer.prompt([
                 {
+                    type: "input",
+                    name: "empAddFirstName",
+                    message:
+                      "What is the first name of the employee you would like to add?",
+                  },
+                  {
+                    type: "input",
+                    name: "empAddLastName",
+                    message:
+                      "What is the last name of the employee you would like to add?",
+                  },
+                  {
+                    type: "number",
+                    name: "empAddMgrId",
+                    message:
+                      "What is the manager ID of the employee you would like to add?",
+                    default: 1,
+                  },
+                {
                     type: 'list',
-                    name: 'role',
+                    name: 'roleId',
                     message: 'What is your role?',
-                    choices: roles
+                    choices: listOfRoles
                 }
             ])
-            .then(rolesChoice => {
-                const role = rolesChoice.roles;
-                parameters.push(roles);
-
-                showEmployees();
-
-           })
+            .then(answer => {
+                connection.query("INSERT INTO Employees SET ?",
+                {
+                  last_name: answer.empAddLastName,
+                  first_name: answer.empAddFirstName,
+                  role_id: answer.roleId,
+                  manager_id: answer.empAddMgrId,
+                },
+                (err, res) => {
+                  if (err) throw err;
+                  showEmployees();
+                }
+              );
+            });
         })
-    })
-}
+    };
+
+
+    // this will delete the employee
+    deleteEmp = () => {
+
+        inquirer
+        .prompt([
+          {
+            name: "empToRemove",
+            type: "input",
+            message:
+              "What is the last name of the employee you would like to remove?",
+          },
+        ])
+        .then((answer) => {
+          const query = `SELECT emp_id AS Employee_ID, first_name AS First_Name, last_name AS Last_Name, title AS Title, salary AS Salary, departments.name AS Department FROM employees 
+          INNER JOIN roles ON employees.role_Id = roles.role_id
+          INNER JOIN departments ON roles.dept_id = departments.dept_id 
+          WHERE ?`;
+          connection.query(query, { last_name: answer.empToRemove }, (err, res) => {
+            if (err) throw err;
+            if (res.length === 0) {
+              console.log (chalk.red.inverse("No employee found by that name"));
+              newTransaction ();
+            } else {
+              console.log(`                              ` + chalk.red.bold(`Employee Information:`));
+              console.table(res);
+
+              inquirer
+                .prompt({
+                name: "idConfirm",
+                type: "number",
+                message: "Please enter the employee's ID to confirm choice:",
+                })
+                .then((answer) => {
+                  const query = "SELECT * FROM Employees WHERE ?";
+                  connection.query(query, { emp_id: answer.idConfirm }, (err, res) => {
+                  if (err) throw err;
+                  let idToDelete = answer.idConfirm;
+                  const deleteQuery = `DELETE FROM employees WHERE emp_id = ${idToDelete}`;
+                  connection.query(deleteQuery, (err,res) => {
+                    if (err) throw err;
+                    console.log('Deleted employee');
+                    showEmployees();
+                  })
+                }
+                );
+                });
+            }
+        }
+        );
+        });
+        
+    }
